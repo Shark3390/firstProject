841983836a8e49eee4b56bbd0d1f637ca5295d99
Shark3390 <126410045+Shark3390@users.noreply.github.com>
2024-01-06 13:01:58 -0500

    Update Test.java with a comment

--- src/com/smart/tests/Test.java
+++ src/com/smart/tests/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	@org.testng.annotations.Test
 	public void loginTest() {
 		
-		
+		//ADDING A COMMENT
 		System.out.println("We are learning GIT and GitHub through Eclipse.");
 		System.out.println("we had our initial commit.");
 	}
